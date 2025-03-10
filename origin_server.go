b5a991a78f6d054799222973e32462a25a43faff
biswassri <srijoni.biswas1994@gmail.com>
2025-03-09 21:33:59 -0400

    add(reverse-proxy): create a http-reverse-proxy

---
+++ origin_server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+)
+
+func runOriginServer() {
+
+	originServerHandler := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		fmt.Printf("[origin server] received request at:%s originServerHandler %s %s\n ", time.Now(), req.URL.Path, req.Method)
+		//Record response from Server
+		_, err := fmt.Fprint(w, "origin server response")
+		if err != nil {
+			log.Fatal(err)
+		}
+	})
+	//Custom server for monitoring incoming requests
+	s := &http.Server{
+		Addr:         ":8081",
+		Handler:      originServerHandler,
+		ReadTimeout:  10 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  15 * time.Second,
+	}
+	//Origin Server running on port:8081
+	log.Printf("Starting origin server on %s ", s.Addr)
+	err := s.ListenAndServe()
+	log.Printf("Server on %s exited: %v", s.Addr, err)
+
+}
